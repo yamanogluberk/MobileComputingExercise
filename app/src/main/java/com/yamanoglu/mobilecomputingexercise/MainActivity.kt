9c0e5cdfca095d0b969faaebb4433e2febc9806d
Berk Yamanoglu <berk.yamanoglu@solidict.com>
2020-01-27 14:27:13 +0300

    homework 2

--- app/src/main/java/com/yamanoglu/mobilecomputingexercise/MainActivity.kt
+++ app/src/main/java/com/yamanoglu/mobilecomputingexercise/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.yamanoglu.mobilecomputingexercise
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fab_time.setOnClickListener {
+            startActivity(Intent(this, TimeActivity::class.java))
+        }
+        fab_map.setOnClickListener{
+            startActivity(Intent(this, MapActivity::class.java))
+        }
+
+        var fabopened = false
+        fab.setOnClickListener {
+            if (!fabopened){
+                fabopened = true
+                fab_time.animate().translationY(-resources.getDimension(R.dimen.standart_66))
+                fab_map.animate().translationY(-resources.getDimension(R.dimen.standart_116))
+            }else{
+                fabopened = false
+                fab_time.animate().translationY(0f)
+                fab_map.animate().translationY(0f)
+
+            }
+        }
     }
 }
